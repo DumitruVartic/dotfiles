8e375e7ba418ea07a1c1128c8fbe4556fce015bb
DumitruVartic <104945638+DumitruVartic@users.noreply.github.com>
2024-08-31 21:33:00 +0300

    Added waybar config

--- createsymlink.sh
+++ createsymlink.sh
@@ -1,1 +1,14 @@
 ln -sf $HOME/dotfiles/Configs/.config/hypr/ $HOME/.config/
+ln -sf $HOME/dotfiles/Configs/.config/waybar/ $HOME/.config/
+
+Scripts="$HOME/dotfiles/Configs/.local/share/bin/" 
+Scripts_Target="$HOME/.local/share/bin/"
+# Copy all files from the source to the target directory
+for file in "$Scripts"/*; do
+  if [ -f "$file" ]; then
+    # cp "$file" "$Scripts_Target/"
+    ln -sf "$file" "$Scripts_Target/"
+  else
+    echo "Warning: No files found in $SOURCE_DIR"
+  fi
+done
